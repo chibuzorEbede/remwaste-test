6ab312392631e3d5262dda3614a1bf63a78e6aae
Chibuzor Ebede <chibuzorebede@gmail.com>
2025-06-07 22:55:55 +0100

    ready

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 const Button = ({ text, skipData }) => {
+  //add to cart functionality
+  // onClick={() => console.log(skipData)}
   return (
-    <button
-      className="px-4 py-2 capitalize bg-[#2E7D32] text-white rounded-full hover:cursor cursor-pointer"
-      onClick={() => console.log(skipData)}
-    >
+    <button className="px-4 py-2 capitalize bg-[#2E7D32] text-white rounded-full hover:cursor cursor-pointer">
       {text}
     </button>
   );
